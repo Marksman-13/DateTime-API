756e162fdadff9d35488b9b1def91e709d75d8a8
Achmed Inderbiev <achmedinderbiev@gmail.com>
2022-06-22 19:48:58 +0200

    Initial commit

---
+++ src/test/java/at/inderbiev/datetimeapi/DateTimeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package at.inderbiev.datetimeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DateTimeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
